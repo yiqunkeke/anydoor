a1bc74c5bc91b6c7db4886a130e74456deee08b2
unknown <1431806069@qq.com>
2020-04-01 17:26:34 +0800

    config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "no-console": [ "error"]
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # anydoor
 Tiny NodeJS Static Web server
+
+## 使用方法
